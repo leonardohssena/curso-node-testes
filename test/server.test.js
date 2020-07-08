4840a638a428159b5431161d6e843230df390ba2
Leonardo Sena <leonardohssena@gmail.com>
2020-07-08 09:50:29 -0300

    Respondendo na raiz

--- test/server.test.js
+++
@@ -1,9 +0,0 @@
-const supertest = require('supertest')
-const request = supertest('http://localhost:3001')
-
-test('Deve responder na porta 3001', async () => {
-  // Acessar a url http://localhost:3001
-  const res = await request.get('/')
-  // Verificar que a resposta foi 200
-  expect(res.status).toBe(200)
-})
