9cfab420002371c0276bb93690d0d969afa18146
Leonardo Sena <leonardohssena@gmail.com>
2020-07-20 15:11:49 -0300

    Criando arquivo de rotas

--- src/app.js
+++ src/app.js
@@ -3,23 +3,14 @@ import consign from 'consign'
 
 const app = express()
 
-consign({ cwd: 'src', verbose: false })
+consign({ cwd: 'src' })
   .include('./config/middlewares.js')
+  .then('./routes')
+  .then('./config/routes.js')
   .into(app)
 
 app.get('/', (req, res) => {
   res.status(200).send()
-})
-
-app.get('/users', (req, res) => {
-  const users = [
-    { name: 'John Doe', mail: 'john@mail.com' }
-  ]
-  res.status(200).json(users)
-})
-
-app.post('/users', (req, res) => {
-  res.status(201).json(req.body)
 })
 
 export default app
