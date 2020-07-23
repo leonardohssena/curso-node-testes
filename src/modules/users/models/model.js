970062e8b98a811885835064de416716d9266a58
Leonardo Sena <leonardohssena@gmail.com>
2020-07-23 10:04:07 -0300

    Add database

---
+++ src/modules/users/models/model.js
@@ -0,0 +1,18 @@
+export default {
+  name: {
+    type: String,
+    trim: true,
+    required: true
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true
+  }
+}
